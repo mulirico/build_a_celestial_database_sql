48220e59ecf8bae0aff82677b6c5de1443166820
mulirico <104590143+mulirico@users.noreply.github.com>
2022-05-18 18:54:22 -0300

    Add files via upload

---
+++ universe.sql
@@ -0,0 +1,45 @@
+Last login: Wed May 18 18:49:11 on ttys001
+murillo@MacBook-Air-de-Murillo ~ % vim
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+--
+-- Name: star star_galaxy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+~                                                                               
+
